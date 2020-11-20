21d330de174d3ada380f77c7e00fc1d682c45ce5
Powertr1p <sc2aru@gmail.com>
2020-11-20 12:32:46 +0300

    fixed gravity and prefabs material

---
+++ Assets/Scripts/DominoFigure.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DominoFigure : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.GetComponent<DominoFigure>())
+        {
+            _audioSource.Play();
+        }
+    }
+}
