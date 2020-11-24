e7018302181bdf53caa4adf67cd2400e527805a2
Powertr1p <sc2aru@gmail.com>
2020-11-24 15:00:21 +0300

    created logic for spawn

---
+++ Assets/Scripts/Player/Raycaster.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Player
+{
+    public class Raycaster : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefab;
+
+        private Vector3 _spawnPosition;
+        private Transform _lastSpawnedFigure;
+
+        private void Update()
+        {
+            TryRaycast();
+        }
+
+        private void TryRaycast()
+        {
+            if (Input.GetMouseButton(0))
+                if (CanPlaceFigure())
+                    Spawn(_spawnPosition);
+        }
+
+        private bool CanPlaceFigure()
+        {
+            RaycastHit[] hits = Physics.SphereCastAll(GetMouseRay(), 0.3f, Mathf.Infinity);
+            if (hits.Length == 0) return false;
+            
+            foreach (var hit in hits)
+            {
+                if (!hit.collider.CompareTag("Ground") || hit.collider.GetComponent<DominoFigure>() != null)
+                    return false;
+            }
+
+            _spawnPosition = hits[0].point;
+            return true;
+        }
+
+        private void Spawn(Vector3 position)
+        {
+            var domino = Instantiate(_prefab, position + Vector3.up, Quaternion.identity);
+            
+            if (_lastSpawnedFigure != null)
+                domino.transform.LookAt(_lastSpawnedFigure);
+
+            _lastSpawnedFigure = domino.transform;
+        }
+        
+        private Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
