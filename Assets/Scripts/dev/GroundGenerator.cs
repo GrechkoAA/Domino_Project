942282c093d24f88122c1d300dc52d4374132de3
Powertr1p <sc2aru@gmail.com>
2020-12-09 22:59:39 +0300

    created functionality for generating level

--- Assets/Scripts/dev/GroundGenerator.cs
+++ Assets/Scripts/dev/GroundGenerator.cs
@@ -11,11 +11,12 @@ namespace dev
         [SerializeField] private int[] _placeableSections;
 
         [SerializeField] private GameObject _ground;
+        [SerializeField] private GameObject _placeableGround;
         [SerializeField] private bool _place;
         [SerializeField] private bool _deleteAll;
 
         private List<GameObject> _createdObjectPool;
-        
+
         private void Update()
         {
             if (_place)
@@ -37,12 +38,25 @@ namespace dev
                 var instance = Instantiate(_ground, newPosition, Quaternion.identity);
                 _createdObjectPool.Add(instance);
             }
+
+            if (_placeableSections.Length <= 0) return;
+
+            for (int j = 0; j < _placeableSections.Length; j++)
+            {
+                _createdObjectPool.RemoveAt(_placeableSections[j]);
+                DestroyImmediate(_createdObjectPool[_placeableSections[j]].gameObject);
+                Vector3 newPosition = new Vector3(transform.position.x + GetComponent<MeshRenderer>().bounds.size.x * _placeableSections[j], transform.position.y, transform.position.z);
+                var instance = Instantiate(_placeableGround, newPosition, Quaternion.identity);
+                _createdObjectPool.Insert(_placeableSections[j], instance);
+            }
         }
 
         private void DeleteObjects()
         {
             _deleteAll = false;
 
+            if (_createdObjectPool.Count <= 0) return;
+            
             foreach (var obj in _createdObjectPool)
                 DestroyImmediate(obj.gameObject);
         }
