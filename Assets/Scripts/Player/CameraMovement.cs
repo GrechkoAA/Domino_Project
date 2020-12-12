b2cec76b0192ae948c3ad87df7b8db1265b7ec75
Powertr1p <sc2aru@gmail.com>
2020-12-12 15:14:24 +0300

    added logic to move camera

---
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Player
+{
+    public class CameraMovement : MonoBehaviour
+    {
+        [SerializeField] private Transform _movingDirection;
+        [SerializeField] private bool _isMoving;
+        [SerializeField] private float _speed = 1f;
+        
+
+        private void Update()
+        {
+            if (_isMoving)
+                MoveCamera();
+        }
+
+        private void MoveCamera()
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _movingDirection.position, _speed * Time.deltaTime);
+        }
+    }
+}
