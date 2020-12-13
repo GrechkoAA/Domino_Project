9beb75095bd3938e0baa9a2f7a75da672ec090e5
Powertr1p <sc2aru@gmail.com>
2020-12-13 12:20:12 +0300

    figures now change color when colliding

--- Assets/Scripts/Figure/DominoFigure.cs
+++ Assets/Scripts/Figure/DominoFigure.cs
@@ -1,30 +1,49 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace Figure
 {
     [RequireComponent(typeof(AudioSource))]
     [RequireComponent(typeof(Transform))]
+    [RequireComponent(typeof(MeshRenderer))]
     public class DominoFigure : MonoBehaviour
     {
         private AudioSource _audioSource;
         private Transform _transform;
+        private MeshRenderer _mesh;
+        private Material _material;
 
         private void Awake()
         {
             _audioSource = GetComponent<AudioSource>();
             _transform = GetComponent<Transform>();
+            _mesh = GetComponent<MeshRenderer>();
         }
 
         private void OnCollisionEnter(Collision other)
         {
             if (other.collider.GetComponent<DominoFigure>())
+            {
                 _audioSource.Play();
+                StartCoroutine(ChangeColor());
+            }
         }
-
+        
         public void ApplyRotation(Transform rotateTo)
         {
             _transform.LookAt(rotateTo);
             _transform.eulerAngles = new Vector3(0f, _transform.eulerAngles.y, 0f);
+        }
+
+        private IEnumerator ChangeColor()
+        {
+            _material = _mesh.material;
+            while (_material.color != Color.yellow)
+            {
+                _material.color = Color.Lerp(_material.color, Color.yellow, 0.05f);
+                yield return new WaitForSeconds(0f);
+            }
         }
     }
 }
