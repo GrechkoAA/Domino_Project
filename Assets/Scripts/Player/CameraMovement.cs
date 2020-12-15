d95dfb5eb22abe0153e9d22cbf6b37dcbd0b6dee
Powertr1p <sc2aru@gmail.com>
2020-12-15 18:52:49 +0300

    camera now moves only on X position

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -8,8 +8,9 @@ namespace Player
         [SerializeField] private Transform _movingDirection;
         [SerializeField] private bool _isMoving;
         [SerializeField] private float _speed = 1f;
-        
 
+        private Vector3 _lastSavedPosition;
+        
         private void Update()
         {
             if (_isMoving)
@@ -18,7 +19,9 @@ namespace Player
 
         private void MoveCamera()
         {
+            _lastSavedPosition = transform.position;
             transform.position = Vector3.MoveTowards(transform.position, _movingDirection.position, _speed * Time.deltaTime);
+            transform.position = new Vector3(transform.position.x, _lastSavedPosition.y, _lastSavedPosition.z);
         }
     }
 }
