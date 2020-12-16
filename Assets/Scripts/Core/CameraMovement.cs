b72c5c5005527f3369c7b30b8842b6f2b27ad387
Powertr1p <sc2aru@gmail.com>
2020-12-16 14:12:08 +0300

    moved classes from one namespace to another

--- Assets/Scripts/Core/CameraMovement.cs
+++ Assets/Scripts/Core/CameraMovement.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
-namespace Player
+namespace Core
 {
     public class CameraMovement : MonoBehaviour
     {
--- Assets/Scripts/Core/Spawner.cs
+++ Assets/Scripts/Core/Spawner.cs
@@ -1,23 +1,27 @@
+using System;
 using Figure;
 using UnityEngine;
 
-namespace Player
+namespace Core
 {
     public class Spawner : MonoBehaviour
     {
         [SerializeField] private DominoFigure _prefab;
         [SerializeField] private float _lineBreakDistance = 1f;
-        
+        [SerializeField] private FigureStateHandler _figureHandler;
+
         private DominoFigure _previousInstance;
         
         public void Spawn(Vector3 position)
         {
             var currentInstance = Instantiate(_prefab, position + Vector3.up, Quaternion.identity);
+            currentInstance.transform.SetParent(_figureHandler.transform);
 
             if (_previousInstance != null)
                 if (DistanceBetweenFigures(currentInstance.transform.position, _previousInstance.transform.position) < _lineBreakDistance)
                     RotateSpawnedFigures(currentInstance);
-
+            
+            _figureHandler.HandleCreatedFigure(currentInstance);
             _previousInstance = currentInstance;
         }
         
