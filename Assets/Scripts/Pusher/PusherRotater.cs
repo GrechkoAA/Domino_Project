a44823b09edaf5f6845ec6485d3b44f29b695216
Powertr1p <sc2aru@gmail.com>
2021-01-28 11:23:08 +0300

    cerated pusher and logic for pusher

---
+++ Assets/Scripts/Pusher/PusherRotater.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Pusher
+{
+    public class PusherRotater : MonoBehaviour
+    {
+        [SerializeField] private Transform _pivot;
+        [SerializeField] private float _speed = 150f;
+
+        private void Update()
+        {
+            transform.RotateAround(_pivot.position, _pivot.forward, _speed * Time.deltaTime);
+        }
+
+        private void OnBecameInvisible()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
