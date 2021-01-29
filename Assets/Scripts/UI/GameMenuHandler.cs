20a92dfe145135620dc02496b8b87ccff512c21c
Powertr1p <sc2aru@gmail.com>
2021-01-29 12:24:21 +0300

    create logic for game UI and session handler

---
+++ Assets/Scripts/UI/GameMenuHandler.cs
@@ -0,0 +1,62 @@
+using System;
+using Core;
+using Ground;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class GameMenuHandler : MonoBehaviour
+    {
+        [SerializeField] private GameUIPresenter _presenter;
+        [SerializeField] private GameSession _session;
+        [SerializeField] private FinishGround _finishGround;
+        [SerializeField] private FigureStateHandler _figureHandler;
+
+        private void OnEnable()
+        {
+            _finishGround.LevelFinished += OnLevelComplete;
+            _figureHandler.LevelFailed += OnLevelFailed;
+        }
+        
+        private void OnDisable()
+        {
+            _finishGround.LevelFinished -= OnLevelComplete;
+            _figureHandler.LevelFailed -= OnLevelFailed;
+        }
+
+        public void StartLevel()
+        {
+            _presenter.LevelStart();
+            _session.StartGame();
+        }
+
+        public void RestartLevel()
+        {
+            var currentScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentScene, LoadSceneMode.Single);
+        }
+
+        public void LoadNextLevel()
+        {
+            if (SceneManager.GetActiveScene().buildIndex >= SceneManager.sceneCount - 1)
+                SceneManager.LoadScene(0, LoadSceneMode.Single);
+            else
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1, LoadSceneMode.Single);
+
+        }
+
+        private void OnLevelComplete()
+        {
+            _presenter.ShowLevelCompleted();
+            _session.OnGameOver();
+        }
+
+        private void OnLevelFailed()
+        {
+            _presenter.ShowLevelFailed();
+            _session.OnGameOver();
+        }
+    }
+}
