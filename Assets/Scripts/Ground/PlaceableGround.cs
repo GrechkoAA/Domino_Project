3fa326fcad062f04516c4acd1ddfd5b4346d8769
Powertr1p <sc2aru@gmail.com>
2021-02-03 15:03:31 +0300

    refactored logic of placeable ground

--- Assets/Scripts/Ground/PlaceableGround.cs
+++ Assets/Scripts/Ground/PlaceableGround.cs
@@ -6,58 +6,18 @@ namespace Ground
     [ExecuteAlways]
     public class PlaceableGround : MonoBehaviour
     {
-        [SerializeField] private GameObject _cube;
-        [SerializeField] private bool _generate;
-
         [SerializeField] private Material _greenMaterial;
         [SerializeField] private Material _redMaterial;
-        
-        private int _offset = 2;
-        private int _maximumBlocks = 5;
 
         public void Update()
         {
             if (Application.isPlaying) return;
-            
-            if (_generate)
-            {
-                _generate = false;
-                GeneratePlaceableBlocks();
-            }
-            
+
             if (transform.childCount > 0)
-                ChangeColorIfNotPlaceable();
+                TryChangeColor();
         }
 
-        private void GeneratePlaceableBlocks()
-        {
-            if (transform.childCount > 0)
-                DeleteExistedBlocks();
-
-            CreateNewBlocks();
-        }
-
-        private void DeleteExistedBlocks()
-        {
-            foreach (var block in GetComponentsInChildren<Block>())
-                DestroyImmediate(block);
-        }
-
-        private void CreateNewBlocks()
-        {
-            for (int x = 0; x < _maximumBlocks; x++)
-            {
-                for (int z = 0; z < _maximumBlocks; z++)
-                {
-                    var instance = Instantiate(_cube, transform);
-                    var localPosition = instance.transform.localPosition;
-                    localPosition = new Vector3(localPosition.x + x * _offset, 0, localPosition.z - z * _offset);
-                    instance.transform.localPosition = localPosition;
-                }
-            }
-        }
-
-        private void ChangeColorIfNotPlaceable()
+        private void TryChangeColor()
         {
             foreach (var block in GetComponentsInChildren<Block>())
             {
