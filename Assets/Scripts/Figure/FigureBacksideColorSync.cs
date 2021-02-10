060deacb282be8b13a1a9c69e1607dbf993062ef
Powertr1p <sc2aru@gmail.com>
2021-02-10 14:18:23 +0300

    backside of domino now copies the mesh color from parent

---
+++ Assets/Scripts/Figure/FigureBacksideColorSync.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Figure
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class FigureBacksideColorSync : MonoBehaviour
+    {
+        private DominoFigure _frontSideFigure;
+        private MeshRenderer _backSideRenderer;
+
+        private void Awake()
+        {
+            _frontSideFigure = GetComponentInParent<DominoFigure>();
+            _backSideRenderer = GetComponent<MeshRenderer>();
+        }
+
+        private void Update()
+        {
+            _backSideRenderer.material.color = _frontSideFigure.CurrentColor;
+        }
+    }
+}
