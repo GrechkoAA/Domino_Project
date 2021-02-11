9851f0f29bf5851179c454ef37954c24cb8bb51f
Powertr1p <sc2aru@gmail.com>
2021-02-11 11:38:09 +0300

    incapsulated sound logic in own class

---
+++ Assets/Scripts/Figure/SFXPresenter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Figure
+{
+    [RequireComponent(typeof(Collider))]
+    [RequireComponent(typeof(AudioSource))]
+    public class SFXPresenter : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        public void PlaySound()
+        {
+            _audioSource.Play();
+        }
+        
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+    }
+}
