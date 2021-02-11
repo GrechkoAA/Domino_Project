db5e40efc37fc97adf94c078f092767dd4734a2d
Powertr1p <sc2aru@gmail.com>
2021-02-11 11:36:58 +0300

    incapsulated color change class

---
+++ Assets/Scripts/Figure/ColorChanger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Figure
+{
+    [RequireComponent(typeof(MeshRenderer))]
+    public class ColorChanger : MonoBehaviour
+    {
+        public Color CurrentColor { get; private set; }
+        
+        private MeshRenderer _mesh;
+        private Color _defaultColor;
+
+        private void Awake()
+        {
+            _mesh = GetComponent<MeshRenderer>();
+        }
+        
+        private void Start()
+        {
+            _defaultColor = _mesh.material.color;
+            CurrentColor = _defaultColor;
+        }
+
+        private void OnBecameInvisible()
+        {
+            ChangeColorToDefault();
+        }
+        
+        public void StartChangeColor()
+        {
+            StartCoroutine(ChangeColor());
+        }
+        
+        public void ChangeColorToDefault()
+        {
+            _mesh.material.color = _defaultColor;
+            CurrentColor = _defaultColor;
+        }
+        
+        private IEnumerator ChangeColor()
+        {
+            while (_mesh.material.color != Color.yellow)
+            {
+                CurrentColor = Color.Lerp(_mesh.material.color, Color.yellow, 0.05f); 
+                _mesh.material.color = CurrentColor;
+                yield return new WaitForSeconds(0f);
+            }
+        }
+    }
+}
